8ed640188599924a86db0180fa8ef862a13a6d50
Pecon <admin@pecon.us>
2021-02-24 16:00:35 -0800

    Finished and tested wine-bl.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # stackscript-blockland
 A stackscript for installing wine and blockland on a new debian system
+
+## Requirements
+1 to 2 GB of memory is recommended.
+This script is designed specifically for the Debian 10 x64 distro. Other debian-based distros may work, but there is no guarantee.
--- wine-bl.sh
+++ wine-bl.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 function pause
 {
 	read -n1 -r -p "Press any key to continue..."
@@ -10,31 +11,42 @@ function fail
 	exit
 }
 
-if [ "$EUID" -ne 0 ] then
+if [ "$EUID" -ne 0 ] 
+then
 	echo "This script must be run as root. Use sudo or log in root."
 	fail
 fi
 
 echo "Updating apt..."
-apt update -q
+apt-get -qq update
 
-if [ $? -gt 0 ] then
+if [ $? -gt 0 ] 
+then
 	echo "Failed to update apt. You might just need to run the script again."
 	fail
 fi
 
 echo "Installing essential packages"
-apt install -q -y screen htop bmon curl wget
+apt-get -qq -o=Dpkg::Use-Pty=0 install -y gpg screen htop bmon curl wget
 
-if [ $? -gt 0 ] then
+if [ $? -gt 0 ] 
+then
 	echo "Failed to get basic tools, something is amiss... Continuing anyways though."
 fi
 
 echo "Getting winehq pgp signing key"
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-apk-key add winehq.key
+wget -nv -nc https://dl.winehq.org/wine-builds/winehq.key
 
-if [ $? -gt 0 ] then
+if [ $? -gt 0 ]
+then
+	echo "Failed to download wine pgp key."
+	fail
+fi
+
+apt-key add winehq.key
+
+if [ $? -gt 0 ] 
+then
 	echo "Failed to install wine pgp key. You may need to install it manually with \"apt-key add winehq.key\". winehq.key has been left behind in this directory."
 	fail
 fi
@@ -44,52 +56,89 @@ rm winehq.key
 echo "Enabling i386 package architecture"
 dpkg --add-architecture i386
 
-if [ $? -gt 0 ] then
+if [ $? -gt 0 ] 
+then
 	echo "Failed to enable i386 packages, are you using an incompatible processor?"
 	fail
 fi
 
+echo "Enabling i386 packages in apt sources.list"
+cp /etc/apt/sources.list sources.list.bak
+sed -i 's/deb h/deb [arch=i386,amd64] h/g' /etc/apt/sources.list
+
+if [ $? -gt 0 ]
+then
+	echo "Failed to edit sources.list."
+	fail
+fi
+
+apt-get -qq update
+
+if [ $? -gt 0 ]
+then
+	echo "Failed to update packages after editing sources.list, reverting changes..."
+	cp sources.list.bak /etc/apt/sources.list
+	fail
+else
+	rm sources.list.bak
+fi
+
 echo "Acquiring libfaudio0 from external repository"
-wget "https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/amd64/libfaudio0_20.01-0~buster_amd64.deb"
+wget -nv -nc "https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/amd64/libfaudio0_20.01-0~buster_amd64.deb" "https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/i386/libfaudio0_20.01-0~buster_i386.deb"
 
 STATUS=$?
 
-if [ $STATUS -gt 0 ] then
+if [ $STATUS -gt 0 ]
+then
 
-	if [ $STATUS -eq 8 ] then
+	if [ $STATUS -eq 8 ] 
+	then
 		echo "The download server issued an error response, this script might be out of date."
 		fail
-	elif [ $STATUS -gt 2 ] then
+	elif [ $STATUS -gt 2 ] 
+	then
 		echo "General network error detected. Try again when the network is up."
 		fail
-	elif [ $STATUS -eq 1 ] then
+	elif [ $STATUS -eq 1 ] 
+	then
 		echo "wget exited with generic error code, maybe everything is okay?"
 		pause
 	fi
 fi
 
-apt install -q -y "./libfaudio0_20.01-0~buster_amd64.deb"
+apt-get -qq -o=Dpkg::Use-Pty=0 install -y "./libfaudio0_20.01-0~buster_amd64.deb" "./libfaudio0_20.01-0~buster_i386.deb"
 
-if [ $? -gt 0 ] then
+if [ $? -gt 0 ]
+then
 	echo "Failed to install libfaudio0 package."
 	fail
 fi
 
+rm libfaudio0_20.01-0~buster_amd64.deb
+
 echo "Adding winehq repository"
 echo "deb https://dl.winehq.org/wine-builds/debian/ buster main" > /etc/apt/sources.list.d/winehq.list
-apt update -q
+apt-get -qq update
 
-if [ $? -gt 0 ] then
+if [ $? -gt 0 ] 
+then
 	echo "Failed to update repositories after adding wine repo, removing wine repo just in case..."
 	rm /etc/apt/sources.list.d/winehq.list
 	fail
 fi
 
 echo "Installing wine..."
-apt install --install-recommends -q -y winehq-stable winbind
+apt-get -qq -o=Dpkg::Use-Pty=0 install --install-recommends winehq-stable xvfb
 
 
-if [ $? -gt 0 ] then
+if [ $? -gt 0 ] 
+then
 	echo "Failed to to install wine."
 	fail
 fi
+
+echo "Wine is now installed!"
+
+WINEDEBUG=-all wineboot
+
+echo "Wine setup completed."
